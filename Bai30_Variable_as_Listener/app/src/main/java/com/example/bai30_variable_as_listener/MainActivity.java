b4fab048547793412dae19d982e0f7ac21cfbeb9
zed123123 <github.123123@gmail.com>
2019-09-16 18:55:38 +0700

    Bai30_Variable_as_Listener

--- Bai30_Variable_as_Listener/app/src/main/java/com/example/bai30_variable_as_listener/MainActivity.java
+++ Bai30_Variable_as_Listener/app/src/main/java/com/example/bai30_variable_as_listener/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.bai30_variable_as_listener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAn, btnHienThi;
+    ImageView imgHinh;
+
+    View.OnClickListener myEvent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v == btnAn){
+                imgHinh.setVisibility(View.INVISIBLE);
+            }
+            else if(v == btnHienThi){
+                imgHinh.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnAn.setOnClickListener(myEvent);
+        btnHienThi.setOnClickListener(myEvent);
+    }
+
+    private void addControls() {
+        btnAn = findViewById(R.id.btnAn);
+        btnHienThi = findViewById(R.id.btnHienThi);
+        imgHinh = findViewById(R.id.imgHinh);
     }
 }
