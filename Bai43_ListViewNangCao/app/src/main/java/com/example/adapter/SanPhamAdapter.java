fc4e7a3358b7ee38b50563a45dee9725685bcda5
zed123123 <github.123123@gmail.com>
2019-09-18 15:21:11 +0700

    Bai43_ListViewNangCao

--- Bai43_ListViewNangCao/app/src/main/java/com/example/adapter/SanPhamAdapter.java
+++ Bai43_ListViewNangCao/app/src/main/java/com/example/adapter/SanPhamAdapter.java
@@ -1,4 +1,43 @@
 package com.example.adapter;
 
-public class SanPhamAdapter  {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bai43_listviewnangcao.R;
+import com.example.model.SanPham;
+
+public class SanPhamAdapter  extends ArrayAdapter<SanPham> {
+
+    Activity context;
+    int resource;
+
+    public SanPhamAdapter(Activity context, int resource) {
+        super(context, resource);
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = this.context.getLayoutInflater();
+        View customView = inflater.inflate(this.resource, null);
+
+        ImageView imgHinh = customView.findViewById(R.id.imgHinh);
+        TextView txtTen = customView.findViewById(R.id.txtTen);
+        TextView txtGia = customView.findViewById(R.id.txtGia);
+
+        SanPham sp = getItem(position);
+        imgHinh.setImageResource(sp.getHinh());
+        txtTen.setText(sp.getTen());
+        txtGia.setText(sp.getGia() + "VND");
+
+        return customView;
+    }
 }
--- Bai43_ListViewNangCao/app/src/main/java/com/example/bai43_listviewnangcao/MainActivity.java
+++ Bai43_ListViewNangCao/app/src/main/java/com/example/bai43_listviewnangcao/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.bai43_listviewnangcao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.adapter.SanPhamAdapter;
+import com.example.model.SanPham;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvSanPham;
+    SanPhamAdapter sanPhamAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControls();
+        addEvents();
+        fakeData();
+    }
+
+    private void fakeData() {
+        sanPhamAdapter.add(new SanPham(R.drawable.book1, "Sach 1", 15000));
+        sanPhamAdapter.add(new SanPham(R.drawable.book2, "Sach 2", 30000));
+        sanPhamAdapter.add(new SanPham(R.drawable.book3, "Sach 3", 150000));
+        sanPhamAdapter.add(new SanPham(R.drawable.book4, "Sach 4", 25000));
+        sanPhamAdapter.add(new SanPham(R.drawable.book5, "Sach 5", 50000));
+        sanPhamAdapter.add(new SanPham(R.drawable.book6, "Sach 6", 95000));
+    }
+
+    private void addEvents() {
+        lvSanPham.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SanPham sp = sanPhamAdapter.getItem(position);
+                Toast.makeText(MainActivity.this, sp.getTen(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addControls() {
+        lvSanPham = findViewById(R.id.lvSanPham);
+        sanPhamAdapter = new SanPhamAdapter(MainActivity.this, R.layout.item);
+        lvSanPham.setAdapter(sanPhamAdapter);
     }
 }
